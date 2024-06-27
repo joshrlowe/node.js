3a1724618e186a4be684bd24c2595f48ae9fca17
Josh Lowe <joshlowe.cs@gmail.com>
2024-06-27 09:23:11 -0400

    67 Using Express Handler - Notes

--- 5 express.js/67 Using Express Router/app.js
+++ 5 express.js/67 Using Express Router/app.js
@@ -8,7 +8,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Middleware to use routes defined in separate files
 app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.listen(3000);
+
+/*
+Additional Explanation:
+- Introduced route handlers from external files:
+  - `adminRoutes` and `shopRoutes` are imported from `./routes/admin` and `./routes/shop` respectively.
+  - `app.use(adminRoutes)` and `app.use(shopRoutes)` integrate these routes into the main application.
+*/
