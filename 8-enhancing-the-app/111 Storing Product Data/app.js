bbced0416e751d786353a380ab9c2bddb1161d1c
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-02 14:07:29 -0400

    111 Storing Product Data

---
+++ 8-enhancing-the-app/111 Storing Product Data/app.js
@@ -0,0 +1,20 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+const errorController = require("./controllers/error.js");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
+
+app.listen(3000);
