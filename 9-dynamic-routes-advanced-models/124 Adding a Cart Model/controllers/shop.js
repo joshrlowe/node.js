5decb7610122e9171e27890946fcbbf9940cf0e3
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-03 10:52:55 -0400

    124 Adding a Cart Model

---
+++ 9-dynamic-routes-advanced-models/124 Adding a Cart Model/controllers/shop.js
@@ -0,0 +1,68 @@
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.status(404).render("404", {
+        pageTitle: "Product Not Found",
+        path: "/404",
+      });
+    }
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
