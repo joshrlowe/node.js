866a73db3d42caafce297855108597698b896896
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-03 09:58:07 -0400

    122 Rendering the Product Detail View

---
+++ 9-dynamic-routes-advanced-models/122 Rendering the Product Detail View/controllers/shop.js
@@ -0,0 +1,59 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.status(404).render("404", {
+        pageTitle: "Product Not Found",
+        path: "/404",
+      });
+    }
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
