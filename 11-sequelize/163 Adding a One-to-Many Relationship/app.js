40d1d4e22babf5bb5027f6efc86fa13c291e3f58
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-07 20:07:11 -0400

    163 Adding a One-to-Many Relationship

---
+++ 11-sequelize/163 Adding a One-to-Many Relationship/app.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+// One-to-Many Relationship
+// One User has many Products
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE",
+});
+
+// Define other direction to be verbose
+// One User has many Products
+User.hasMany(Product);
+
+sequelize
+  // Override tables
+  .sync({ force: true })
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
