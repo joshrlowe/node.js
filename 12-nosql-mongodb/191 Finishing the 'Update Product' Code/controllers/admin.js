1b7f9d7519842168f1ea97f6daa8ba72c675600a
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-09 11:34:24 -0400

    191 Finishing the 'Update Product' Code

---
+++ 12-nosql-mongodb/191 Finishing the 'Update Product' Code/controllers/admin.js
@@ -0,0 +1,106 @@
+const Product = require("../models/product");
+
+// Imports the ObjectId class from the mongodb package to work with MongoDB's unique document identifiers.
+const { ObjectId } = require("mongodb");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then((products) => {
+      exports.getProducts = (req, res, next) => {
+        Product.fetchAll()
+          .then((products) => {
+            res.render("admin/products", {
+              prods: products,
+              pageTitle: "Admin Products",
+              path: "/admin/products",
+            });
+          })
+          .catch((err) => console.log(err));
+      };
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(
+    req.body.title,
+    req.body.price,
+    req.body.description,
+    req.body.imageUrl,
+  );
+  product
+    .save()
+    .then((result) => {
+      // console.log(result);
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  Product.findById(req.params.productId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  // Creates a new product instance with the updated form data. Note that it reuses the Product model's constructor
+  // but passes in the existing product ID (from the form) to update the correct product.
+  const product = new Product(
+    req.body.title,
+    req.body.price,
+    req.body.description,
+    req.body.imageUrl,
+    new ObjectId(req.body.productId), // Converts the product ID from the form into an ObjectId for MongoDB operations
+  );
+
+  // Calls the save method on the product instance to update the product in the database.
+  product
+    .save()
+    .then((result) => {
+      console.log("Updated Product!");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// exports.postDeleteProduct = (req, res, next) => {
+//   Product.findByPk(req.body.productId)
+//     .then((product) => {
+//       return product.destroy();
+//     })
+//     .then((result) => {
+//       console.log("Destroyed Product!");
+//       res.redirect("/admin/products");
+//     })
+//     .catch((err) => console.log(err));
+// };
---
+++ 12-nosql-mongodb/191 Finishing the 'Update Product' Code/models/product.js
@@ -0,0 +1,67 @@
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl, id) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id; // Set the product ID
+  }
+
+  save() {
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
+      .then((result) => {
+        // console.log(result); // Log the result of the database operation
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        return new Product(
+          product.title,
+          product.price,
+          product.description,
+          product.imageUrl,
+          product._id,
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = Product;
