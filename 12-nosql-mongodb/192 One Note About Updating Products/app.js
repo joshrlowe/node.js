e1409283e510c46f1cd7b45efaa47f5a5e033f59
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-09 11:39:32 -0400

    192 One Note About Updating Products

---
+++ 12-nosql-mongodb/192 One Note About Updating Products/app.js
@@ -0,0 +1,38 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const mongoConnect = require("./util/database").mongoConnect;
+
+const errorController = require("./controllers/error");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  // Example of fetching user from database, not currently in use
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
+  next();
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
