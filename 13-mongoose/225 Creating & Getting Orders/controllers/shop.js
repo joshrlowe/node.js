bec56dc3b475c6f4207f9505db04508e308cfe7e
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-10 22:55:46 -0400

    225 Creating & Getting Orders

---
+++ 13-mongoose/225 Creating & Getting Orders/controllers/shop.js
@@ -0,0 +1,113 @@
+const Product = require("../models/product");
+const Order = require("../models/order");
+const order = require("../models/order");
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProduct = (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then((product) => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postCart = (req, res, next) => {
+  Product.findById(req.body.productId)
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then(() => {
+      console.log("Added to Cart!");
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  req.user
+    .removeFromCart(req.body.productId)
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const order = new Order({
+        user: {
+          name: req.user.username,
+          userId: req.user,
+        },
+        products: user.cart.items.map((i) => {
+          return { quantity: i.quantity, productData: { ...i.productId._doc } };
+        }),
+      });
+      order.save();
+      console.log("Order Placed!");
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getOrders = (req, res, next) => {
+  Order.find({ "user.userId": req.user._id })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
+};
