09849065409fdb3623137e69fed8266c95e8da33
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-15 19:49:43 -0400

    244 Deleting a Cookie

---
+++ 14-sessions-cookies/244 Deleting a Cookie/controllers/admin.js
@@ -0,0 +1,107 @@
+const Product = require("../models/product");
+const User = require("../models/user");
+
+exports.getAddProduct = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login");
+  }
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product({
+    title: req.body.title,
+    price: req.body.price,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    userId: req.session.user._id,
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log("Created Product!");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login");
+  }
+
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+
+  Product.findById(req.params.productId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  Product.findById(req.body.productId)
+    .then((product) => {
+      product.title = req.body.title;
+      product.price = req.body.price;
+      product.description = req.body.description;
+      product.imageUrl = req.body.imageUrl;
+      return product.save();
+    })
+    .then((result) => {
+      console.log("Updated Product!");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login");
+  }
+
+  Product.find()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  Product.findByIdAndDelete(req.body.productId)
+    .then(() => {
+      return User.updateMany(
+        { "cart.items.productId": req.body.productId },
+        { $pull: { "cart.items": { productId: req.body.productId } } },
+      );
+    })
+    .then(() => {
+      console.log("Deleted Product!");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
