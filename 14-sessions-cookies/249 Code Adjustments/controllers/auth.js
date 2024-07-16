633d4ddf4261af3fc5acfa0fc3fbbdbcc797ceee
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-15 22:52:02 -0400

    249 Code Adjustments

---
+++ 14-sessions-cookies/249 Code Adjustments/controllers/auth.js
@@ -0,0 +1,39 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/login");
+      }
+      if (user.password !== req.body.password) {
+        return res.redirect("/login");
+      }
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+};
