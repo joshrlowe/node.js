0ae0520119a5ebc74501d942cc5a0079a4d2148c
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-16 11:48:09 -0400

    255 Implementing an Authentication Flow

---
+++ 15-authentication/255 Implementing an Authentication Flow/controllers/auth.js
@@ -0,0 +1,70 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/login");
+      }
+      if (user.password !== req.body.password) {
+        return res.redirect("/login");
+      }
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (user) {
+        console.log("User already exists");
+        return res.redirect("/login");
+      }
+      if (password !== confirmPassword) {
+        console.log("Passwords do not match");
+        return res.redirect("/signup");
+      }
+      const newUser = new User({ email: email, password: password, cart: [] });
+      return newUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+};
