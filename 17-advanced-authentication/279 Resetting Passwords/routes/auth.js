116b0d4da238bd1f796c42881b502fc6ada47be9
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-16 16:00:13 -0400

    279 Resetting Passwords

---
+++ 17-advanced-authentication/279 Resetting Passwords/routes/auth.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const authController = require("../controllers/auth");
+const router = express.Router();
+
+// /login => GET
+router.get("/login", authController.getLogin);
+
+// /login => POST
+router.post("/login", authController.postLogin);
+
+// /logout => POST
+router.post("/logout", authController.postLogout);
+
+// /signup => GET
+router.get("/signup", authController.getSignup);
+
+// /signup => POST
+router.post("/signup", authController.postSignup);
+
+// /reset => GET
+router.get("/reset", authController.getReset);
+
+module.exports = router;
