524b24e7cbd6ff9df9901302e135cba54ee53fc4
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-17 13:50:49 -0400

    304 Validating Product Editing

---
+++ 18-understanding-validation/304 Validating Product Editing/controllers/admin.js
@@ -0,0 +1,138 @@
+const Product = require("../models/product");
+const User = require("../models/user");
+
+const { validationResult } = require("express-validator");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      hasError: true,
+      errorMessage: errors.array()[0].msg,
+      product: {
+        title: req.body.title,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        description: req.body.description,
+      },
+      validationErrors: errors.array(),
+    });
+  }
+  const product = new Product({
+    title: req.body.title,
+    price: req.body.price,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    userId: req.user._id,
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log("Created Product!");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+
+  Product.findById(req.params.productId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: false,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      errorMessage: errors.array()[0].msg,
+      product: {
+        title: req.body.title,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        description: req.body.description,
+        _id: req.body.productId,
+      },
+      validationErrors: errors.array(),
+    });
+  }
+  Product.findById(req.body.productId)
+    .then((product) => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect("/");
+      }
+      product.title = req.body.title;
+      product.price = req.body.price;
+      product.description = req.body.description;
+      product.imageUrl = req.body.imageUrl;
+      return product.save().then((result) => {
+        console.log("Updated Product!");
+        res.redirect("/admin/products");
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.find({ userId: req.user._id })
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  Product.deleteOne({ _id: req.body.productId, userId: req.user._id })
+    .then(() => {
+      return User.updateMany(
+        { "cart.items.productId": req.body.productId },
+        { $pull: { "cart.items": { productId: req.body.productId } } },
+      );
+    })
+    .then(() => {
+      console.log("Deleted Product!");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
