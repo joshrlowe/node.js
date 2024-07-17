6fe453c9b28cf2ac14957d8e20acab94aa9329ce
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-17 17:11:28 -0400

    322 Handling Multipart Form Data

---
+++ 20-file-upload-download/322 Handling Multipart Form Data/app.js
@@ -0,0 +1,92 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const csrf = require("csurf");
+const express = require("express");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+const path = require("path");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+const User = require("./models/user");
+const adminRoutes = require("./routes/admin");
+const authRoutes = require("./routes/auth");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
+
+const app = express();
+
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/shop?w=majority&appName=${process.env.DB_NAME}&retryWrites=true`;
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+
+const csrfProtection = csrf();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  }),
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use("/500", errorController.get500);
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+  });
